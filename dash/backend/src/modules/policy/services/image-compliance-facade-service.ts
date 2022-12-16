a863c5d771a26539603383a308a15cdd4ccad4a9
charis <charis.prose@intelletive.com>
2022-12-16 11:41:12 -0600

    Issue Title - name column instead of description column

--- dash/backend/src/modules/policy/services/image-compliance-facade-service.ts
+++ dash/backend/src/modules/policy/services/image-compliance-facade-service.ts
@@ -63,7 +63,7 @@ export class ImageComplianceFacadeService {
                 const override = exceptionsOverride.find(exception => exception.issueIdentifier.toUpperCase() === issue.type.toUpperCase());
                 if(override?.altSeverity){
                     issue.severity = override.altSeverity
-                    issue.description = "overridden - "+ issue.description;
+                    issue.description = "overridden - "+ issue.name;
                 }
             }
         }
