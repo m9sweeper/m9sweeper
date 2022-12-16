1a14eafdc8c21363c59ada82647a179c8fcb0ed6
charis <charis.prose@intelletive.com>
2022-12-16 13:56:44 -0600

    progress on json show data component

--- dash/frontend/src/app/core/dialogues/show-json-data/show-json-data.component.ts
+++ dash/frontend/src/app/core/dialogues/show-json-data/show-json-data.component.ts
@@ -12,7 +12,24 @@ export class ShowJsonDataComponent implements OnInit {
   header: string;
 
   constructor(public dialogRef: MatDialogRef<ShowJsonDataComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: {content: any, header: string}) { }
+              @Inject(MAT_DIALOG_DATA) public data: {content: any, header: string},
+              private route: ActivatedRoute,
+              private falcoService: FalcoService
+  ) { }
+
+  dataSource: MatTableDataSource<IFalcoLog>;
+  displayedColumns = ['calendarDate', 'namespace', 'pod', 'image', 'message'];
+  namespace = this.data.content.namespace;
+  date = this.data.content.calendarDate;
+  pod = this.data.content.container;
+  image = this.data.content.image;
+  message = this.data.content.message;
+  signature = this.data.content.anomalySignature;
+  clusterId = this.data.content.clusterId;
+
+  limit = this.getLimitFromLocalStorage() ? Number(this.getLimitFromLocalStorage()) : 20;
+  logCount: number;
+  page: number;
 
   ngOnInit(): void {
     this.header = this.data.header ? this.data.header : 'Json Data';
@@ -20,6 +37,50 @@ export class ShowJsonDataComponent implements OnInit {
 
   get tableHtml(): string {
     return jsonToTableHtmlString(this.data.content);
+    this.getFalcoEvents();
+  }
+
+  pageEvent(pageEvent: any) {
+    this.limit = pageEvent.pageSize;
+    this.page = pageEvent.pageIndex;
+    this.setLimitToLocalStorage(this.limit);
+    this.getFalcoEvents();
+  }
+
+  getFalcoEvents(){
+    this.falcoService.getFalcoLogs(this.clusterId,  { limit: this.limit, page: this.page, signature: this.signature})
+      .pipe(take(1))
+      .subscribe(response => {
+        this.dataSource = new MatTableDataSource(response.data.list);
+        this.logCount = response.data.logCount;
+      }, (err) => {
+        alert(err);
+      });
+  }
+
+  stripDomainName(image: string): string {
+    const regex = /^([a-zA-Z0-9]+\.[a-zA-Z0-9\.]+)?\/?([a-zA-Z0-9\/]+)?\:?([a-zA-Z0-9\.]+)?$/g;
+    const group = image.split(regex);
+    // strip domain, only image
+    if (group[2] !== undefined && group[3] === undefined){
+      return (group[2]);
+    } else if (group[3] !== undefined){
+      return (group[2] + group [3]);
+    } else if (group[2] === undefined){
+      return '';
+    }
+  }
+
+  getLimitFromLocalStorage(): string | null {
+    return localStorage.getItem('falco_table_limit');
+  }
+
+  setLimitToLocalStorage(limit: number) {
+    localStorage.setItem('falco_table_limit', String(limit));
+  }
+
+  onClickMore(){
+
   }
 
   onClose() {
