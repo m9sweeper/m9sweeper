610943c4fe8c90e92d349e8058a4aa44f3f62f65
charis <charis.prose@intelletive.com>
2023-01-10 18:36:37 -0600

    frontend - add logic to not save if no setting is chosen

--- dash/frontend/src/app/modules/private/pages/falco/falco-settings/falco-settings.component.ts
+++ dash/frontend/src/app/modules/private/pages/falco/falco-settings/falco-settings.component.ts
@@ -49,7 +49,7 @@ export class FalcoSettingsComponent implements OnInit {
 
   }
 
-  onClickSave(){
+  onClickSave() {
 
     const falcoCreatePayload: IFalcoSettingPayload = {
       clusterId: this.clusterId,
@@ -62,26 +62,28 @@ export class FalcoSettingsComponent implements OnInit {
       whoToNotify: this.settingForm.get('whoToNotify').value,
       emailList: this.settingForm.get('emailList').value
     };
-    console.log('clusterid', falcoCreatePayload.clusterId);
-    console.log('type clusterid', typeof(falcoCreatePayload.clusterId));
-    console.log('falcoCreatePayload', falcoCreatePayload.sendNotificationAnomaly);
-    console.log('type sendnotAnom', typeof(falcoCreatePayload.sendNotificationAnomaly));
-    console.log('falcoCreatePayload', falcoCreatePayload.anomalyFrequency);
-    console.log('type anomalyFreq', typeof(falcoCreatePayload.anomalyFrequency));
-    console.log('falcoCreatePayload', falcoCreatePayload.severityLevel);
-    console.log('falcoCreatePayload', falcoCreatePayload.sendNotificationSummary);
-    console.log('falcoCreatePayload', falcoCreatePayload.summaryNotificationFrequency);
-    console.log('falcoCreatePayload', falcoCreatePayload.weekday);
-    console.log('falcoCreatePayload', falcoCreatePayload.whoToNotify);
-    console.log('falcoCreatePayload', falcoCreatePayload.emailList);
 
-    this.falcoService.addFalcoSetting(this.clusterId, falcoCreatePayload).subscribe((response: IServerResponse<IFalcoSettingPayload>) => {
-      this.alertService.success('Setting saved successfully.');
-      // window.location.reload();
-    }, (event) => {
-      this.alertService.danger(event.error.message);
-    });
+    if (
+      falcoCreatePayload.sendNotificationAnomaly === null &&
+      falcoCreatePayload.anomalyFrequency === null &&
+      falcoCreatePayload.severityLevel.length === 0 &&
+      falcoCreatePayload.sendNotificationSummary === null &&
+      falcoCreatePayload.summaryNotificationFrequency === null &&
+      falcoCreatePayload.weekday === null &&
+      falcoCreatePayload.whoToNotify === null &&
+      falcoCreatePayload.emailList.length === 0) {
+
+      this.alertService.warning('No setting is chosen');
+
+    } else {
+      this.falcoService.addFalcoSetting(this.clusterId, falcoCreatePayload).subscribe((response: IServerResponse<IFalcoSettingPayload>) => {
+        this.alertService.success('Setting saved successfully.');
+        // window.location.reload();
+      }, (event) => {
+        this.alertService.danger(event.error.message);
+      });
     }
+  }
 
   onClickEdit(){
 
