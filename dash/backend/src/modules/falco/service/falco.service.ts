1f00514e8d4df37756a6ce6929b236b3cef0e489
charis <charis.prose@intelletive.com>
2023-01-19 15:18:34 -0600

    backend update

--- dash/backend/src/modules/falco/service/falco.service.ts
+++ dash/backend/src/modules/falco/service/falco.service.ts
@@ -179,7 +179,7 @@ export class FalcoService {
         // get all admin email addresses
         const allAdminEmail = await this.getAllAdminsToMail();
         // To test email: const allAdminEmailArray = ['some_email_address'] and comment out forEach();
-        const allAdminEmailArray = [''];
+        const allAdminEmailArray = [];
         allAdminEmail.forEach( element => allAdminEmailArray.push(element.email));
 
         // Parse data from new falco log fields
