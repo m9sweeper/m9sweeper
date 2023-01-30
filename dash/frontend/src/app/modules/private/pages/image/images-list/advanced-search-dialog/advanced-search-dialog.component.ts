b2b01db8a25b899e49c0b1cf689826d2b6602ab5
charisprose <56838011+charisprose@users.noreply.github.com>
2023-01-30 17:08:05 -0600

    Advanced Search - Remember previous input data (#33)
    * remember what was entered
    
    * save previous image too
    
    * update
    
    * remember checkbox

--- dash/frontend/src/app/modules/private/pages/image/images-list/advanced-search-dialog/advanced-search-dialog.component.ts
+++ dash/frontend/src/app/modules/private/pages/image/images-list/advanced-search-dialog/advanced-search-dialog.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AlertService} from '@full-fledged/alerts';
+import {MatCheckboxChange} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-advanced-search-dialog',
@@ -11,6 +12,7 @@ import {AlertService} from '@full-fledged/alerts';
 export class AdvancedSearchDialogComponent implements OnInit {
 
   searchForm: FormGroup;
+  isChecked = false;
 
   constructor(private dialogRef: MatDialogRef<AdvancedSearchDialogComponent>,
               private formBuilder: FormBuilder,
@@ -22,15 +24,40 @@ export class AdvancedSearchDialogComponent implements OnInit {
     this.searchForm = this.formBuilder.group({
       name: [''],
       cve: [''],
-      onlyRunning: [true]
+      onlyRunning: []
     });
+
+    const searchName = localStorage.getItem('image-search-name');
+    if (searchName){
+      this.searchForm.get('name').setValue(searchName);
+    }
+    const searchCve = localStorage.getItem('image-search-cve');
+    if (searchCve){
+      this.searchForm.get('cve').setValue(searchCve);
+    }
+    const searchRunningImage = JSON.parse(localStorage.getItem('image-search-running-image'));
+    this.searchForm.get('onlyRunning').setValue(searchRunningImage);
   }
 
   onSearch() {
+    const storageArray = [];
+
+    const searchImageText = this.searchForm.get('name').value;
+    localStorage.setItem('image-search-name', searchImageText);
+    const searchCveText = this.searchForm.get('cve').value;
+    localStorage.setItem('image-search-cve', searchCveText);
+
+    const searchRunningImage = this.searchForm.get('onlyRunning').value;
+    localStorage.setItem('image-search-running-image', JSON.stringify(searchRunningImage));
+
     this.dialogRef.close(this.searchForm.value);
   }
 
   onCancel() {
     this.dialogRef.close();
+  }
+
+  onlyRunningImage(event: MatCheckboxChange){
+   this.isChecked = !this.isChecked;
   }
 }
