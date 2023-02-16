6daeb6b3888162fd680f9670f6c2dbcb239fab44
charis <charis.prose@intelletive.com>
2023-02-16 17:39:42 -0600

    backend - nestjs & node

--- dash/backend/src/modules/user/user.module.ts
+++ dash/backend/src/modules/user/user.module.ts
@@ -2,8 +2,6 @@ import { Global, Module } from '@nestjs/common';
 import { UserDao } from './dao/user.dao';
 import { UserProfileService } from './services/user-profile.service';
 import { UserController } from './controllers/user.controller';
-import {EmailService} from '../shared/services/email.service';
-import {ResetPasswordService} from '../auth/services/reset-password.service';
 
 @Global()
 @Module({
@@ -16,8 +14,6 @@ import {ResetPasswordService} from '../auth/services/reset-password.service';
     UserProfileService
   ],
   imports: [
-    EmailService,
-    ResetPasswordService
   ],
   controllers: [
     UserController
