45af4085a39325cec46cf3eb011694a26144d49e
Jacob Beasley <jacobbeasley@gmail.com>
2023-02-28 09:12:45 -0600

    supporting large port numbers in install wizard and switching to apiRegistration V1 API because v1beta1 was removed in version 1.22 of kubernetes (#53)

--- dash/frontend/src/app/modules/private/pages/cluster/add-cluster-wizard/add-cluster-wizard.component.ts
+++ dash/frontend/src/app/modules/private/pages/cluster/add-cluster-wizard/add-cluster-wizard.component.ts
@@ -37,7 +37,7 @@ export class AddClusterWizardComponent implements OnInit {
   installWebhook = 'automatically';
   nextClusterId: number;
   baseUrl: string;
-  validatorsPatternForPortNumber = /^()([1-9]|[1-5]?[0-9]{2,4}|6[1-4][0-9]{3}|65[1-4][0-9]{2}|655[1-2][0-9]|6553[1-5])$/;
+  validatorsPatternForPortNumber = /^\d+$/;
   @ViewChild('defaultWebhookTextArea') defaultWebhookTextArea: ElementRef;
 
   constructor( private dialogRef: MatDialogRef<AddClusterWizardComponent>,
