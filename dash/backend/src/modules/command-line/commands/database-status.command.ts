b1e468cbe3cdd62e1f4e9d33ae4ee88c7351996b
Brandan Schmitz <brandan.schmitz@celestialdata.net>
2023-06-13 17:25:28 -0500

    Modify database status command

--- dash/backend/src/modules/command-line/commands/database-status.command.ts
+++ dash/backend/src/modules/command-line/commands/database-status.command.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { Command } from 'nestjs-command';
-import { Client } from 'pg';
+import { DatabaseService } from '../../shared/services/database.service';
 
 /**
  * This command is used mainly by init containers and stuff to verify database connectivity before attempting to start pods to helm with errors
  */
 @Injectable()
 export class DatabaseStatusCommand {
+
+    // Create the constructor so that we can utilize the DatabaseService for this command
+    constructor(private readonly databaseService: DatabaseService) {}
+
+    /**
+     * Run a check against the defined database to ensure it is responding to queries.
+     */
     @Command({command: 'database:status', describe: 'Checks to ensure the database is online and responding to queries.' })
-    async runCheck(): Promise<void> {
-        // Create the database client using the most basic PG library to avoid any other errors potentially being
-        // thrown somewhere else that can screw up the check. This also keeps it extremly light weight.
-        const client = new Client({
-            host: process.env.DATABASE_CONNECTION_HOST,
-            port: process.env.DATABASE_CONNECTION_PORT,
-            database: process.env.DATABASE_CONNECTION_DATABASE,
-            user: process.env.DATABASE_CONNECTION_USERNAME,
-            password: process.env.DATABASE_CONNECTION_PASSWORD,
-        });
-
-        // Attempt the connect to the databse and run the SELECT 1 query commonly used to verify databse connectivity
-        // If the query succesds then return a 0 status code and exit otherwise return a status code of 1 showing it could not connect.
+    async runCheck() {
         try {
-            await client.conect();
-            await client.query('SELECT 1');
-            await client.end();
+            // Create a connection to the database
+            const connection = await this.databaseService.getConnection();
 
-            console.log("Database is responding to queries at this time.");
+            // Exectue a simple statement against the database to verify it is responding to queries
+            await connection.raw("SELECT 1");
+
+            // Display that the database is online and responding and ensure we exit the command with a clean return code
+            console.log("Database is online and responding");
             process.exit(0);
         } catch (error) {
-            console.error('Database is not responding to queries at this time.');
+            // Log out any error we get and ensure the command exits wit a failed return code
+            console.error('Unable to connect to the databse: ', error.message);
             process.exit(1);
         }
     }
