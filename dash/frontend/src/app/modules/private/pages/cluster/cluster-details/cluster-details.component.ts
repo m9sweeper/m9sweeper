fa40248cc48cc249402cd30c62a1ac74e77da545
charis <charis.prose@intelletive.com>
2023-06-16 19:05:43 -0500

    cluster-details class name was wrong, no longer need to widen page content width, clean up unused code

--- dash/frontend/src/app/modules/private/pages/cluster/cluster-details/cluster-details.component.ts
+++ dash/frontend/src/app/modules/private/pages/cluster/cluster-details/cluster-details.component.ts
@@ -3,8 +3,6 @@ import {ActivatedRoute, Router} from '@angular/router';
 import { ClusterService } from '../../../../../core/services/cluster.service';
 import { faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import {SharedSubscriptionService} from '../../../../../core/services/shared.subscription.service';
-import {ClusterGroupService} from '../../../../../core/services/cluster-group.service';
-import {JwtAuthService} from '../../../../../core/services/jwt-auth.service';
 
 @Component({
   selector: 'app-cluster-details',
@@ -21,11 +19,7 @@ export class ClusterDetailsComponent implements OnInit {
   cluster: any;
   groupId: any;
   isClusterLoaded = false;
-  height: number;
-  width: number;
-  isExpanded: boolean;
-  isSmallSize: boolean;
-  isSmallDevice = false;
+
   leftNavWidth: number;
   clusterDashWidth: number;
 
@@ -37,64 +31,8 @@ export class ClusterDetailsComponent implements OnInit {
   {}
 
   ngOnInit(): void {
-    /*
-    this.isExpanded = localStorage.getItem('expand') ? JSON.parse(localStorage.getItem('expand')) : true;
-    this.width = document.documentElement.clientWidth;
-    this.height = document.documentElement.clientHeight;
-    this.screenSizeExpand(this.width);
-    document.documentElement.style.setProperty('--cluster-details-height', `${this.height}px`);
-    */
-    this.widenClusterDetailsContentPage();
+
     this.getClusterById(this.route.snapshot.params.id);
-  }
-  /*
-  @HostListener('window:resize', ['$event']) // listen for screen size change
-  calculateScreenSize($event?: any) {
-    this.scrHeight = document.documentElement.clientHeight;
-    this.scrWidth = document.documentElement.clientWidth;
-    this.isSmallDevice = false;
-    // document.documentElement.style.setProperty('--cluster-details-height', `${this.height}px`);
-    // document.documentElement.style.setProperty('--dashboard-details-width', `${this.width}px`);
-    // this.screenSizeExpand(this.scrWidth);
-  }
-
-  set scrHeight(val: number) {
-    if (val !== this.height) {
-      this.height = val;
-    }
-  }
-
-  get scrHeight(): number {
-    return this.height;
-  }
-
-  set scrWidth(val: number) {
-    if (val !== this.width) {
-      this.width = val;
-    }
-  }
-
-  get scrWidth(): number {
-    return this.width;
-  }
-  menubarCollapse(){
-    if (this.isSmallDevice){
-      this.expand();
-    }
-  }
-  */
-
-  widenClusterDetailsContentPage(){
-    // individual pages on cluster-details dashboard display with page content width
-    // make pages to fill the dashboard width
-
-    // get the current cluster-details dashboard width
-    const clusterDash = document.querySelector('.cluster-details-dashboard');
-    this.clusterDashWidth = clusterDash.clientWidth;
-    // find the cluster-details dashboard's associated class
-    const clusterDashTag = document.getElementById('cluster-dashboard');
-    // use default width from css
-    clusterDashTag.className = 'cluster-dashboard';
   }
 
   expand() {
@@ -115,61 +53,8 @@ export class ClusterDetailsComponent implements OnInit {
       leftNavTag.className = 'cluster-group-menu';
       clusterDashTag.className = 'cluster-dashboard';
     }
-    /*
-    if (this.isSmallSize) {
-      this.isSmallDevice = !this.isSmallDevice;
-      this.expandMenuBarForSmallDevice(this.width);
-    }
-    else {
-      this.isExpanded = !this.isExpanded;
-      localStorage.setItem('expand', `${this.isExpanded}`);
-      this.screenSizeExpand(this.width);
-    }
-    this.sharedSubscriptionService.setCurrentExpandStatus(this.isExpanded);
-    */
 
   }
-  /*
-  expandMenuBarForSmallDevice(width: number) {
-    let menuWidth: number;
-    let containerWidthForSmallDevice: number;
-    if (this.isSmallDevice){
-      this.isExpanded = true;
-      menuWidth = 300;
-      containerWidthForSmallDevice =  width;
-    }
-    else {
-      this.isExpanded = false;
-      menuWidth = 65;
-      containerWidthForSmallDevice = width - menuWidth;
-    }
-    // document.documentElement.style.setProperty('--navigation-menu-width', `${menuWidth}px`);
-    // document.documentElement.style.setProperty('--cluster-container-width', `${containerWidthForSmallDevice}px`);
-  }
-  screenSizeExpand(width: number) {
-    let menuWidth: number;
-    let containerWidth: number;
-
-    if (width <= 800) {
-      menuWidth = 65;
-      containerWidth = width - menuWidth;
-      this.isSmallSize = true;
-      this.isExpanded = false;
-    } else {
-      this.isSmallSize = false;
-      this.isSmallDevice = false;
-      if (this.isExpanded) {
-        menuWidth = 300;
-        containerWidth =  width - menuWidth;
-      } else {
-        menuWidth = 65;
-        containerWidth = width - menuWidth;
-      }
-    }
-    // document.documentElement.style.setProperty('--navigation-menu-width', `${menuWidth}px`);
-    // document.documentElement.style.setProperty('--cluster-container-width', `${containerWidth}px`);
-  }
-   */
   getClusterById(clusterId: number) {
     this.clusterService.getClusterById(clusterId).subscribe(response => {
       this.isClusterLoaded = true;
