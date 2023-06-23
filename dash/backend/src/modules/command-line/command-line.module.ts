865acd39c4bcaf45aba6b9eaa63a137e8917a467
Brandan Schmitz <brandan.schmitz@intelletive.com>
2023-06-23 17:19:59 -0500

    Use init containers (#109)
    * Use init containers
    
    * Add database status check node script and use dash image for init
    
    * Update init containers
    
    * Modify database status command
    
    * Update commands

--- dash/backend/src/modules/command-line/command-line.module.ts
+++ dash/backend/src/modules/command-line/command-line.module.ts
@@ -4,15 +4,16 @@ import { ClusterSyncCommand } from './commands/cluster-sync.command';
 import { KubernetesClusterService } from './services/kubernetes-cluster.service';
 import { KubernetesHistoryCommand } from './commands/kubernetes-history.command';
 import { KubernetesHistoryService } from './services/kubernetes-history.service';
-import {HelmSetupCommand} from "./commands/helm-setup.command";
-import {GatekeeperExceptionCommand} from "./commands/gatekeeper-exception.command";
-import {SyncExceptionStatusCommand} from "./commands/exception.command";
-import {ExceptionBlockService} from "./services/exception-block.service";
+import { HelmSetupCommand } from "./commands/helm-setup.command";
+import { GatekeeperExceptionCommand } from "./commands/gatekeeper-exception.command";
+import { SyncExceptionStatusCommand } from "./commands/exception.command";
+import { ExceptionBlockService } from "./services/exception-block.service";
 import { ImageRescanningService } from './services/image-rescanning.service';
-import {JobsCliController} from './controllers/jobs-cli.controller';
-import {CronJobsController} from './controllers/cron-jobs.controller';
-import {CliCommandBuilderService} from './services/cli-command-builder.service';
-import {ScheduleModule} from '@nestjs/schedule';
+import { JobsCliController } from './controllers/jobs-cli.controller';
+import { CronJobsController } from './controllers/cron-jobs.controller';
+import { CliCommandBuilderService } from './services/cli-command-builder.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { DatabaseStatusCommand } from './commands/database-status.command';
 
 @Global()
 @Module({
@@ -29,6 +30,7 @@ import {ScheduleModule} from '@nestjs/schedule';
         GatekeeperExceptionCommand,
         SyncExceptionStatusCommand,
         ImageRescanningService,
+        DatabaseStatusCommand,
         CliCommandBuilderService
     ],
     imports: [
