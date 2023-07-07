b4eb5be8d28b05838db42cdf05cecc451ab05fd5
Kyle Berndt <kyle.berndt@intelletive.com>
2023-07-07 09:22:08 -0500

    Fix org settings duplidate left nav and extra space

--- dash/frontend/src/app/modules/private/pages/organization-settings/organization-settings.component.ts
+++ dash/frontend/src/app/modules/private/pages/organization-settings/organization-settings.component.ts
@@ -1,58 +1,10 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import { faAngleDoubleLeft, faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-organization-settings',
   templateUrl: './organization-settings.component.html',
   styleUrls: ['./organization-settings.component.scss']
 })
-export class OrganizationSettingsComponent implements OnInit {
+export class OrganizationSettingsComponent {
 
-  faIcons: any = {
-    arrowDoubleLeft: faAngleDoubleLeft,
-    arrowDoubleRight: faAngleDoubleRight
-  };
-
-  leftNavWidth: number;
-  mainDivWidth: number;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {}
-
-  @HostListener('window:resize', ['$event'])
-  collapseIfSmallScreen() {
-    const mainDiv = document.querySelector('.main-container');
-    this.mainDivWidth = mainDiv.clientWidth;
-
-    const leftNavTag = document.getElementById('left-nav');
-    const clusterDashTag = document.getElementById('cluster-dashboard');
-
-    if (this.mainDivWidth < 800 ){
-      leftNavTag.className += ' responsive';
-      clusterDashTag.className += ' responsive';
-    }
-  }
-
-  expand() {
-    // collapse or expand left nav
-
-    // get the current left nav menu width    // get the current left nav menu width
-    const leftNav = document.querySelector('.cluster-group-menu');
-    this.leftNavWidth = leftNav.clientWidth;
-    // find associated class by id
-    const leftNavTag = document.getElementById('left-nav');
-    const clusterDashTag = document.getElementById('cluster-dashboard');
-    // check whether to expand or collapse left nav
-    if (this.leftNavWidth === 300){
-      leftNavTag.className += ' responsive';
-      clusterDashTag.className += ' responsive';
-    }else{
-      leftNavTag.className = 'cluster-group-menu';
-      clusterDashTag.className = 'cluster-dashboard';
-    }
-    // Emit a window resize event to trigger UI handlers listening for resize events
-    window.dispatchEvent(new Event('resize'));
-  }
 }
