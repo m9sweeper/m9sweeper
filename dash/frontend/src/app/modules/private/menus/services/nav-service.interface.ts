c3b2372c8851f7ef30d01afc697b3d4caa00586d
Becky Saunders <becky.saunders@intelletive.com>
2023-07-11 09:53:10 -0500

    + ability to configure routerLinkActiveOptions for menu items (--> reports child pages keep "reports" highlighted in the sidenav)

--- dash/frontend/src/app/modules/private/menus/services/nav-service.interface.ts
+++ dash/frontend/src/app/modules/private/menus/services/nav-service.interface.ts
@@ -1,7 +1,5 @@
 import {IMenuContentTrigger} from '../../../shared/side-nav/interfaces/menu-content-trigger.interface';
 import {IMenuItem} from '../../../shared/side-nav/interfaces/menu-item.interface';
-import {ClusterGroupService} from '../../../../core/services/cluster-group.service';
-import {MatDialog} from '@angular/material/dialog';
 import {BehaviorSubject} from 'rxjs';
 
 export interface NavServiceInterface {
