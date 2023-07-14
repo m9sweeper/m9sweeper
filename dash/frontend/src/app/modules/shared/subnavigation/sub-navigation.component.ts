c14dce4067ea5180bfa0bc2a9b6b2c5997ffeaba
Kyle Berndt <kyle.berndt@intelletive.com>
2023-07-14 13:21:22 -0500

    fix sub navigation component styles

--- dash/frontend/src/app/modules/shared/subnavigation/sub-navigation.component.ts
+++ dash/frontend/src/app/modules/shared/subnavigation/sub-navigation.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sub-navigation',
   templateUrl: './sub-navigation.component.html',
   styleUrls: ['./sub-navigation.component.scss']
 })
-export class SubNavigationComponent implements OnInit {
+export class SubNavigationComponent {
   @Input() buttonTitle: string;
   @Input() buttonUrl: any;
   @Input() title: string;
-  constructor() {}
-
-  ngOnInit(): void {
-  }
 }
