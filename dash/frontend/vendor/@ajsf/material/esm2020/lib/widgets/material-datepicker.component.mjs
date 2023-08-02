f165bc7bf59fc86460c07fc46364e02d68fafd98
becky-intelletive <50845926+becky-intelletive@users.noreply.github.com>
2023-08-02 15:56:00 -0500

    Continued Cleanup (#224)
    * cleaned up gatekeeper list page
    
    * styling cleanups; gatekeeper constraint details cleanup
    
    * update asjf styling to default material appearances to "fill" (was throwing an error)
    
    * cleanup the cleanup

--- dash/frontend/vendor/@ajsf/material/esm2020/lib/widgets/material-datepicker.component.mjs
+++ dash/frontend/vendor/@ajsf/material/esm2020/lib/widgets/material-datepicker.component.mjs
@@ -25,7 +25,7 @@ export class MaterialDatepickerComponent {
 }
 MaterialDatepickerComponent.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "15.2.9", ngImport: i0, type: MaterialDatepickerComponent, deps: [{ token: MAT_FORM_FIELD_DEFAULT_OPTIONS, optional: true }, { token: i1.JsonSchemaFormService }], target: i0.ɵɵFactoryTarget.Component });
 MaterialDatepickerComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "15.2.9", type: MaterialDatepickerComponent, selector: "material-datepicker-widget", inputs: { layoutNode: "layoutNode", layoutIndex: "layoutIndex", dataIndex: "dataIndex" }, ngImport: i0, template: `
-    <mat-form-field [appearance]="options?.appearance || matFormFieldDefaultOptions?.appearance || 'standard'"
+    <mat-form-field [appearance]="options?.appearance || matFormFieldDefaultOptions?.appearance || 'fill'"
                     [class]="options?.htmlClass || ''"
                     [floatLabel]="options?.floatLabel || matFormFieldDefaultOptions?.floatLabel || (options?.notitle ? 'never' : 'auto')"
                     [hideRequiredMarker]="options?.hideRequired ? 'true' : 'false'"
@@ -77,7 +77,7 @@ MaterialDatepickerComponent.ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.
 i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "15.2.9", ngImport: i0, type: MaterialDatepickerComponent, decorators: [{
             type: Component,
             args: [{ selector: 'material-datepicker-widget', template: `
-    <mat-form-field [appearance]="options?.appearance || matFormFieldDefaultOptions?.appearance || 'standard'"
+    <mat-form-field [appearance]="options?.appearance || matFormFieldDefaultOptions?.appearance || 'fill'"
                     [class]="options?.htmlClass || ''"
                     [floatLabel]="options?.floatLabel || matFormFieldDefaultOptions?.floatLabel || (options?.notitle ? 'never' : 'auto')"
                     [hideRequiredMarker]="options?.hideRequired ? 'true' : 'false'"
