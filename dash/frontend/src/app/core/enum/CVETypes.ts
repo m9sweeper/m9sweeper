5031e8670a087126eeb6f6536adce960ce072e4b
Becky Saunders <becky.saunders@intelletive.com>
2023-08-07 10:50:10 -0500

    "running vulnerabilities" table cleaned up

---
+++ dash/frontend/src/app/core/enum/CVETypes.ts
@@ -0,0 +1,16 @@
+
+export enum CVETypes {
+  CRITICAL = 'critical',
+  MAJOR = 'major',
+  MEDIUM = 'medium',
+  LOW = 'low',
+  NEGLIGIBLE = 'negligible',
+}
+
+export enum CVETypeAbbreviations {
+  CRITICAL = 'crt',
+  MAJOR = 'maj',
+  MEDIUM = 'med',
+  LOW = 'min',
+  NEGLIGIBLE = 'neg',
+}
