c3647e176e1a097c0a8fb7e8a71226ee72e8433d
Kyle Berndt <kyle.berndt@intelletive.com>
2023-08-11 12:38:07 -0500

    fix kube hunter results page

--- dash/frontend/src/app/modules/private/pages/cluster/kube-hunter/kube-hunter.component.ts
+++ dash/frontend/src/app/modules/private/pages/cluster/kube-hunter/kube-hunter.component.ts
@@ -19,7 +19,6 @@ export class KubeHunterComponent implements OnInit, OnDestroy {
 
   private unsubscribe$ = new Subject<void>();
   clusterId: number;
-  daysPassed: number;
   allReportsForCluster: IKubeHunterReport[];
   scansExist = false;
   mostRecentVulnerabilities = {low: 0, medium: 0, high: 0};
@@ -64,42 +63,16 @@ export class KubeHunterComponent implements OnInit, OnDestroy {
         take(1),
       )
       .subscribe(res => {
-        if (res.reportCount > 0) {
-          this.scansExist = true;
-          res.list.forEach(reports => {
-            reports.nodes = JSON.parse(reports.nodes);
-            reports.services = JSON.parse(reports.services);
-            reports.vulnerabilities = JSON.parse(reports.vulnerabilities);
-          });
-          this.allReportsForCluster = res.list;
-          if (res.list[0] != null){
-            this.daysPassed = Math.floor((Date.now() - res.list[0].createdAt) / (1000 * 60 * 60 * 24));
-            if (this.daysPassed >= 90) {
-              this.ourAdvice = 'It has been ' + this.daysPassed + ' days since you ran kube-hunter. You should run it again soon.';
-              this.penetrationTestStatusInvalid = true;
-              this.penetrationTestText = 'Report Outdated';
-            } else if (this.daysPassed >= 10) {
-              this.ourAdvice = 'It has been ' + this.daysPassed + ' days since you last ran kube-hunter.';
-              this.penetrationTestStatusInvalid = true;
-              this.penetrationTestText = 'Report Outdated';
-            } else {
-              this.ourAdvice = 'It has been ' + this.daysPassed + ' days since you last ran kube-hunter.';
-              this.penetrationTestStatusInvalid = false;
-              this.penetrationTestText = 'Report Valid';
-            }
-            res.list[0].vulnerabilities?.value?.value?.forEach( vulnerability => {
-              if (vulnerability.severity === 'low') {
-                this.mostRecentVulnerabilities.low += 1;
-              } else if (vulnerability.severity === 'medium') {
-                this.mostRecentVulnerabilities.medium += 1;
-              } else if (vulnerability.severity === 'high') {
-                this.mostRecentVulnerabilities.high += 1;
-              }
-            });
-          }
-          this.reportCount = res.reportCount;
-          this.populateTable();
-        }
+        this.scansExist = !!res?.list?.length;
+        this.allReportsForCluster = res?.list || [];
+        this.reportCount = res?.reportCount || 0;
+        res?.list?.forEach(reports => {
+          reports.nodes = JSON.parse(reports.nodes);
+          reports.services = JSON.parse(reports.services);
+          reports.vulnerabilities = JSON.parse(reports.vulnerabilities);
+        });
+        this.populateMessages(res?.list[0]);
+        this.populateTable();
       }, (e) => {
         if (e.status === 404) {
           this.ourAdvice = 'Run kube-hunter to find vulnerabilities within your cluster.';
@@ -110,9 +83,46 @@ export class KubeHunterComponent implements OnInit, OnDestroy {
   }
 
   populateTable() {
-    this.allReportsForCluster?.forEach(reports => reports.date = new Date(+reports.createdAt).toLocaleString());
     this.dataSource = new MatTableDataSource(this.allReportsForCluster);
   }
+
+  populateMessages(recentScan: IKubeHunterReport): void {
+    // Reset vulnerability counts
+    this.mostRecentVulnerabilities.high = this.mostRecentVulnerabilities.medium
+      = this.mostRecentVulnerabilities.low = 1;
+
+    if (!recentScan) {
+      this.ourAdvice = 'Run kube-hunter to audit your cluster security';
+      this.penetrationTestText = 'Not yet run';
+      this.penetrationTestStatusInvalid = true;
+      return;
+    }
+    // Calculate the messaging for the penetration test status & Our advice column based on
+    // how long it has been since the most recent scan
+    const daysPassed = Math.floor((Date.now() - recentScan.createdAt) / (1000 * 60 * 60 * 24));
+    if (daysPassed >= 90) {
+      this.ourAdvice = 'It has been ' + daysPassed + ' days since you ran kube-hunter. You should run it again soon.';
+      this.penetrationTestStatusInvalid = true;
+      this.penetrationTestText = 'Report Outdated';
+    } else if (daysPassed >= 10) {
+      this.ourAdvice = 'It has been ' + daysPassed + ' days since you last ran kube-hunter.';
+      this.penetrationTestStatusInvalid = true;
+      this.penetrationTestText = 'Report Outdated';
+    } else {
+      this.ourAdvice = 'It has been ' + daysPassed + ' days since you last ran kube-hunter.';
+      this.penetrationTestStatusInvalid = false;
+      this.penetrationTestText = 'Report Valid';
+    }
+    recentScan.vulnerabilities?.value?.value?.forEach(vulnerability => {
+      if (vulnerability.severity === 'low') {
+        this.mostRecentVulnerabilities.low += 1;
+      } else if (vulnerability.severity === 'medium') {
+        this.mostRecentVulnerabilities.medium += 1;
+      } else if (vulnerability.severity === 'high') {
+        this.mostRecentVulnerabilities.high += 1;
+      }
+    });
+}
 
   openDialog() {
     const dialog = this.dialog.open(KubeHunterDialogComponent, {
