cd6649973c67e4484eaaeda868fc673c17999cf6
becky-intelletive <50845926+becky-intelletive@users.noreply.github.com>
2023-08-22 21:21:34 +0000

    + screenshots for debugging

--- selenium-testing/test/specs/cluster/gatekeeper.spec.ts
+++ selenium-testing/test/specs/cluster/gatekeeper.spec.ts
@@ -140,11 +140,14 @@ describe('Gatekeeper Page::', () => {
             pending("Gatekeeper is already setup")
         }
 
+        await browser.customScreenshot("before-setup-button-displays");
+
         // Wait for the button to display
         // @ts-ignore
         await $("//button[contains(text(), 'Setup')]")
             .waitForDisplayed({timeout: 60000, interval: 1000, timeoutMsg: "Setup button was not displayed; This suggests that the API is either failing or taking too long"});
 
+        await browser.customScreenshot("after-setup-button-displays");
         // Locate the setup button and click it
         // @ts-ignore
         await $("//button[contains(text(), 'Setup')]").customClick("setup");
