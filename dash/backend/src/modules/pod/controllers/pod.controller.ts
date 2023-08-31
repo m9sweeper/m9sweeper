83c5fc13ce96a80090ce4efe89ac17fc704be916
Kyle Berndt <kyle.berndt@intelletive.com>
2023-08-31 15:07:50 -0500

    now ensure gatekeeper is set up before getting gatekeeper violations in get k8s-pod endpoint

--- dash/backend/src/modules/pod/controllers/pod.controller.ts
+++ dash/backend/src/modules/pod/controllers/pod.controller.ts
@@ -23,6 +23,7 @@ import { PodComplianceSummaryDto } from '../dto/pod-compliance-summary-dto';
 import { GatekeeperService } from '../../gatekeeper/services/gatekeeper.service';
 import {ClusterService} from '../../cluster/services/cluster.service';
 import {ClusterGroupService} from '../../cluster-group/services/cluster-group-service';
+import {MineLoggerService} from '../../shared/services/mine-logger.service';
 
 @ApiTags('Pods')
 @Controller()
@@ -35,6 +36,7 @@ export class PodController {
       private readonly gatekeeperService: GatekeeperService,
       protected readonly clusterService: ClusterService,
       protected readonly clusterGroupService: ClusterGroupService,
+      protected readonly logger: MineLoggerService,
     ) {}
 
     @Get()
@@ -51,10 +53,17 @@ export class PodController {
       @Query('page') page?: number,
       @Query('sort') sort?: {field: string; direction: string; },
     ): Promise<PodDto[]>{
-        const violations = await this.gatekeeperService.getTotalViolations(clusterId);
         const pods =  await this.podService.getAllPods(clusterId, namespace, sort, page, limit);
-        if (pods && pods.length) {
-            pods.map(p => p.violations = violations.filter(v => v.name === p.name));
+        const gatekeeperStatus = await this.gatekeeperService.getInstallationInfo(clusterId);
+        if (gatekeeperStatus.status) {
+            const violations = await this.gatekeeperService.getTotalViolations(clusterId)
+              .catch((err) => {
+                  this.logger.error('Could not retrieve gatekeeper violations', err, 'PodController.getAllPods');
+                  return [];
+              })
+            if (pods?.length && violations?.length) {
+                pods.map(p => p.violations = violations.filter(v => v.name === p.name));
+            }
         }
         return pods;
     }
