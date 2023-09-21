8cc75697b1e899cb154a6314b34d1c8f99ffdc9e
Becky Saunders <becky.saunders@intelletive.com>
2023-09-21 11:32:56 -0500

    fix spacing on loggedinuserfactory (no other changes)

--- dash/backend/src/providers/LoggedInUserFactory.ts
+++ dash/backend/src/providers/LoggedInUserFactory.ts
@@ -1,33 +1,35 @@
-import {Scope} from '@nestjs/common';
-import {REQUEST} from '@nestjs/core';
-import {JwtUtilityService} from '../modules/auth/services/jwt-utility.service';
-import {MineLoggerService} from '../modules/shared/services/mine-logger.service';
-import {ApiKeyService} from '../modules/api-key/services/api-key.service';
+import { Scope } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { JwtUtilityService } from '../modules/auth/services/jwt-utility.service';
+import { MineLoggerService } from '../modules/shared/services/mine-logger.service';
+import { ApiKeyService } from '../modules/api-key/services/api-key.service';
 
 export default {
-    provide: 'LOGGED_IN_USER',
-    scope: Scope.REQUEST,
-    useFactory: async (request,
-                       jwtUtilityService: JwtUtilityService,
-                       apiKeyService: ApiKeyService,
-                       mineLoggerService: MineLoggerService) => {
-        const authorization = request.get('Authorization')?.split(' ')[1] || '';
-        const apiKey = request.get('x-api-key');
-        try {
-            let userInfo: any;
-            if (authorization) {
-                userInfo =  await jwtUtilityService.verify(authorization);
-            } else if (apiKey) {
-                userInfo = await apiKeyService.getUserInfoByApiKey(apiKey);
-            } else {
-                userInfo = '';
-            }
-            return userInfo;
-        } catch (e) {
-            mineLoggerService.log(e.message, 'LOGGED_IN_USER_FACTORY');
-        }
+  provide: 'LOGGED_IN_USER',
+  scope: Scope.REQUEST,
+  useFactory: async (
+    request,
+    jwtUtilityService: JwtUtilityService,
+    apiKeyService: ApiKeyService,
+    mineLoggerService: MineLoggerService
+  ) => {
+    const authorization = request.get('Authorization')?.split(' ')[1] || '';
+    const apiKey = request.get('x-api-key');
+    try {
+      let userInfo: any;
+      if (authorization) {
+        userInfo = await jwtUtilityService.verify(authorization);
+      } else if (apiKey) {
+        userInfo = await apiKeyService.getUserInfoByApiKey(apiKey);
+      } else {
+        userInfo = '';
+      }
+      return userInfo;
+    } catch (e) {
+      mineLoggerService.log(e.message, 'LOGGED_IN_USER_FACTORY');
+    }
 
-        return null;
-    },
-    inject: [REQUEST, JwtUtilityService, ApiKeyService, MineLoggerService],
-}
+    return null;
+  },
+  inject: [ REQUEST, JwtUtilityService, ApiKeyService, MineLoggerService ],
+};
