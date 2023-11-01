195de92f3b131b9cc3b63e1e2dd10802bdc532ce
Kyle Berndt <kyle.berndt@intelletive.com>
2023-11-01 12:34:34 -0500

    fix assets config

--- dash/backend/src/config/assets.ts
+++ dash/backend/src/config/assets.ts
@@ -2,7 +2,7 @@ import {assetsConfig} from './types/assets-config';
 import * as process from 'process';
 
 
-const assetsDirectory = process.cwd() + '/' + (process.env.ASSESTS_DIRECTORY || 'dist/assets');
+const assetsDirectory = process.cwd() + '/' + (process.env.ASSETS_DIRECTORY || 'assets');
 
 export default {
   assetsDirectory: assetsDirectory,
