6a549bb1a6cdc9c10560b1a9a7c3f1b5eedabdec
gtarique <gazi.tarique@intelletive.com>
2024-02-05 18:34:25 +0600

    Upgrade angular and angular material 15 to 17

--- dash/frontend/src/polyfills.ts
+++ dash/frontend/src/polyfills.ts
@@ -55,8 +55,9 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
+// github.com/angular/angular/blob/main/packages/zone.js/CHANGELOG.md#breaking-changes
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
